ab5b54f02d1c060fe4a9656a7890480abca9abc4
clement <clement.deriencourt@orange.fr>
2022-02-24 11:40:27 +0100

    first

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void testDoFizzBuzz_NumberDivisibleBy3_ShouldReturnFizz(){
+        int nombre = 9;
+        String outputExpected = "Fizz";
+        FizzBuzz fb = new FizzBuzz();
+
+        String result = fb.doFizzBuzz(nombre);
+        Assert.assertEquals(outputExpected, result);
+    }
+
+    @Test
+    public void testDoFizzBuzz_NumberDivisibleBy5_ShouldReturnBuzz(){
+        int nombre = 25;
+        String outputExpected = "Buzz";
+        FizzBuzz fb = new FizzBuzz();
+
+        String result = fb.doFizzBuzz(nombre);
+        Assert.assertEquals(outputExpected, result);
+    }
+
+    @Test
+    public void testDoFizzBuzz_NumberDivisibleBy3And5_ShouldReturnFizzBuzz(){
+        int nombre = 15;
+        String outputExpected = "FizzBuzz";
+        FizzBuzz fb = new FizzBuzz();
+
+        String result = fb.doFizzBuzz(nombre);
+        Assert.assertEquals(outputExpected, result);
+    }
+
+    @Test
+    public void testDoFizzBuzz_NumberNotDivisibleBy3Or5_ShouldReturnEmpty(){
+        int nombre = 2;
+        String outputExpected = "2";
+        FizzBuzz fb = new FizzBuzz();
+
+        String result = fb.doFizzBuzz(nombre);
+        Assert.assertEquals(outputExpected, result);
+    }
+}
